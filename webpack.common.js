befcfb1fa3330d808a7b79c16dacf3fd29b0e6f8
Guillaume Royer <guroyer.dev@gmail.com>
2018-10-03 10:44:03 -0400

    Removed unused dependency. Remove yarn.lock from .gitignore file

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -13,12 +12,11 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
-    resolve: { extensions: ["*", ".ts", ".tsx", ".js"] },
+    resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
     module: {
         rules: [
             { 
                 test: /\.tsx?$/,
-                exclude: /(node_modules)/,
                 loader: "awesome-typescript-loader" 
             },
             { 
